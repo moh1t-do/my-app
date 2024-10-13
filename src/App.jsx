43b5a645705661714ce2a973c5e9f61a7d2f990d
Mohit <mohitd2288@gmail.com>
2024-10-13 17:15:17 +0530

    feat: understood memo

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,41 @@
-import { useState, memo } from "react";
+import { useState, useMemo, memo, useCallback } from "react";
 
 function App() {
   const [count, setCount] = useState(0);
+  const [n, setN] = useState(0);
 
   function handleClick() {
     setCount(count + 1);
   }
 
+  function handleInputChange(event) {
+    setN(event.target.value);
+  }
+
+  const greet = useCallback(() => {
+    return "Hello There";
+  }, [])
+
+
   return (
     <>
-      <button onClick={handleClick}>{`Click Me !! ${count}`}</button>
+      <input type="number" value={n} onChange={handleInputChange} />
+      <button onClick={handleClick}>{`Click Me !!`}</button>
+      <Greet greet={greet} />
+      <Header title={`Current count is ${count}`} />
       <Header title="Hi There 1" />
-      <Header title="Hi There 2" />
     </>
   );
 }
 
 const Header = memo(function ({ title }) {
-  console.log("Header Rendered", title);
+  console.log('Render from', title);
   return <h1>{title}</h1>;
 });
+
+const Greet = memo(function ({ greet }) {
+  console.log('Render from', greet);
+  return <h1>{greet()}</h1>
+})
 
 export default App;
