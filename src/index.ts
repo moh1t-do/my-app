abe4b4c54c5ed0da0b97c966a7eebb2df85ef7d2
Mohit <mohitd2288@gmail.com>
2024-10-21 22:19:44 +0530

    refactor: Add UserRecord and usersMap for user management

--- src/index.ts
+++ src/index.ts
@@ -21,6 +21,40 @@ const aman: Readonly<User> = {
     password: 'lkjll'
 }
 
+// record
+type UserRecord = Record<string, User>
+
+const users: UserRecord = {
+    "raq1": {
+        name: 'Raq',
+        age: 13,
+        email: 'kljk',
+        id: 12,
+        password: 'kljkl'
+    }
+}
+
+// map
+const usersMap = new Map<string, User>()
+usersMap.set('raq1', {
+    name: 'Raq',
+    age: 13,
+    email: 'kljk',
+    id: 12,
+    password: 'kljkl'
+})
+
+console.log(usersMap.get('raq1'));
+
+type EventType = 'click' | 'mouseover' | 'mouseout';
+type ExcludeEvent = Exclude<EventType, 'click'>;
+
+const handleEvent = (event: ExcludeEvent) => {
+    console.log(event);
+}
+
+handleEvent('click');
+
 function updateUser(updateProps: UpdateProps) {
     console.log(`Name: ${updateProps.name}`);
 }
